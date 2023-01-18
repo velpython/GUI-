37817f8b344d60b4d02e7b2cfe86283f2dd81d79
VEL_Anora <velmurugan@anorasolutions.com>
2023-01-18 16:33:37 +0530

    fill the unique clr to buttons

--- qt designer traning/UI/pqt5_file/main_runner.py
+++ qt designer traning/UI/pqt5_file/main_runner.py
@@ -1,9 +1,11 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+import iconify  as ico
+# from iconify.qt import QtGui, QtWidgets
 # from pyside2 import QtWidgets
 import main
 import sys
 
-class MyQtApp(main.Ui_MainWindow,QtWidgets.QMainWindow):
+class MyQtApp(main.Ui_MainWindow,QtWidgets.QMainWindow ):
     def __init__(self):
         # app = QtWidgets.QApplication(sys.argv)
         # MainWindow = QtWidgets.QMainWindow()
@@ -21,10 +23,22 @@ class MyQtApp(main.Ui_MainWindow,QtWidgets.QMainWindow):
         self.tableWidget.setColumnWidth(0,100)
         self.tableWidget.setColumnWidth(1,150)
         self.tableWidget.setColumnWidth(2,500)
+        self.tableWidget_2.setColumnWidth(0,350)
+        self.tableWidget_2.setColumnWidth(1,350)
+        self.tableWidget_2.setColumnWidth(2,500)
         self.load_data()
 
     def load_data(self):
-        people = [{"no " : "1" , "name" : "resister" , "Description " : "used to resist the current " } , {"no " : "2","name" : "force" , "Description " : "force the current "} ]
+        people = [{"no " : "1" , "name" : "resister" , "Description " : "used to resist the current " } ,
+        {"no " : "2","name" : "force" , "Description " : "NA "},
+        {"no " : "3","name" : "voltage" , "Description " : "NA "},
+        {"no " : "4","name" : "capactor" , "Description " : "NA "},
+        {"no " : "5","name" : "inside" , "Description " : "NA "},
+        {"no " : "6","name" : "netural" , "Description " : "NA "},
+        {"no " : "7","name" : "postive" , "Description " : "NA "}]
+
+                    
+                
         row = 0
         self.tableWidget.setRowCount(len(people))
         for person in people:
@@ -39,9 +53,18 @@ class MyQtApp(main.Ui_MainWindow,QtWidgets.QMainWindow):
         width = 1100
         self.setFixedHeight(height)
         self.setFixedWidth(width)
-        self.frameGeometry().width()       
+        self.frameGeometry().width()
+
+    def icon_setting(self):
+        # icon = ico.Icon('feather:loader',color=QtGui.QColor('salmon'),anim =anim)
+        # self.pushButton_3.setIcon(icon)
+        # # self.pushButton(icon)
+        # button = QpushButton 
+        # anim.start()     
+        pass
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     qt_app = MyQtApp()
     qt_app.show()
+    anim = ico.anim.Spin()
     app.exec_()
