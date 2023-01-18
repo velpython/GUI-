8add9e968a54fb65929b907b55fcd7b4ab1b6a2e
VEL_Anora <velmurugan@anorasolutions.com>
2023-01-18 12:07:58 +0530

    implemented the load_data function  for fill  details in respected column's

--- qt designer traning/UI/pqt5_file/main_runner.py
+++ qt designer traning/UI/pqt5_file/main_runner.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+# from pyside2 import QtWidgets
 import main
 import sys
 
@@ -17,8 +18,21 @@ class MyQtApp(main.Ui_MainWindow,QtWidgets.QMainWindow):
         self.populate_tree_widget()
 
     def populate_tree_widget(self):
-        pass
+        self.tableWidget.setColumnWidth(0,100)
+        self.tableWidget.setColumnWidth(1,150)
+        self.tableWidget.setColumnWidth(2,500)
+        self.load_data()
 
+    def load_data(self):
+        people = [{"no " : "1" , "name" : "resister" , "Description " : "used to resist the current " } , {"no " : "2","name" : "force" , "Description " : "force the current "} ]
+        row = 0
+        self.tableWidget.setRowCount(len(people))
+        for person in people:
+            self.tableWidget.setItem(row, 0 , QtWidgets.QTableWidgetItem(person["no "]))
+            self.tableWidget.setItem(row, 1 , QtWidgets.QTableWidgetItem(person["name"]))
+            self.tableWidget.setItem(row, 2 , QtWidgets.QTableWidgetItem(person["Description "]))
+            row = row+1
+            
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     qt_app = MyQtApp()
