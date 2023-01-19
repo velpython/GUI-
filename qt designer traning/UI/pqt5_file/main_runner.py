c4202d979295ecc4b2c7a22ec294995a14e66d9a
VEL_Anora <velmurugan@anorasolutions.com>
2023-01-19 17:12:38 +0530

    created new gui with save and check_box funalities

--- qt designer traning/UI/pqt5_file/main_runner.py
+++ qt designer traning/UI/pqt5_file/main_runner.py
@@ -1,34 +1,34 @@
 from PyQt5 import QtCore, QtGui, QtWidgets 
 import iconify  as ico
-from PyQt5.QtWidgets import QApplication , QRadioButton , QWidget
+from PyQt5.QtWidgets import QApplication , QRadioButton , QWidget , QPushButton , QHBoxLayout , QFileDialog
 # from iconify.qt import QtGui, QtWidgets
 # from pyside2 import QtWidgets
 import main
 import sys
+import os 
 
 class MyQtApp(main.Ui_MainWindow,QtWidgets.QMainWindow ):
     def __init__(self):
-        # app = QtWidgets.QApplication(sys.argv)
-        # MainWindow = QtWidgets.QMainWindow()
-        # self.ui = main.Ui_MainWindow()
-        # self.ui.setupUi(MainWindow)
-        # MainWindow.show()
-        # sys.exit(app.exec_())
         super(MyQtApp,self).__init__()
         self.setupUi(self)
         self.showMaximized()
         self.setWindowTitle("MY GUI")
-        self.icon_setting()
         self.populate_tree_widget()
-        self.radio1.toggled.connect(lambda:self.bgcolor())
+        self.pushButton_10.clicked.connect(self.save_dialog)
+        self.QcheckBox.clicked.connect(self.Q_checkBox)
+        self.QcheckBox_2.clicked.connect(self.Q_checkBox_2)
+        self.QcheckBox_3.clicked.connect(self.Q_checkBox_3)
+        self.QcheckBox_4.clicked.connect(self.Q_checkBox_4)
+        self.QcheckBox_5.clicked.connect(self.Q_checkBox_5)
+        self.QcheckBox_6.clicked.connect(self.Q_checkBox_6)
+        self.QcheckBox_7.clicked.connect(self.Q_checkBox_7)
+        self.load_data()
+        
 
     def populate_tree_widget(self):
         self.tableWidget.setColumnWidth(0,100)
         self.tableWidget.setColumnWidth(1,150)
         self.tableWidget.setColumnWidth(2,500)
-        self.tableWidget_2.setColumnWidth(0,350)
-        self.tableWidget_2.setColumnWidth(1,350)
-        self.tableWidget_2.setColumnWidth(2,500)
         self.load_data()
 
     def load_data(self):
@@ -39,7 +39,6 @@ class MyQtApp(main.Ui_MainWindow,QtWidgets.QMainWindow ):
         {"no " : "5","name" : "inside" , "Description " : "NA "},
         {"no " : "6","name" : "netural" , "Description " : "NA "},
         {"no " : "7","name" : "postive" , "Description " : "NA "}]
-        
                 
         row = 0
         self.tableWidget.setRowCount(len(people))
@@ -50,24 +49,71 @@ class MyQtApp(main.Ui_MainWindow,QtWidgets.QMainWindow ):
             row = row+1
             self.size_increase()
     def size_increase(self):
-        self.setStyleSheet("QTabBar::tab { height: 30px; width: 100px; background: 'yellow'}") 
+        self.setStyleSheet("QTabBar::tab_2 { height: 30px; width: 100px; background: 'yellow'}") 
         height = 600
         width = 1100
         self.setFixedHeight(height)
         self.setFixedWidth(width)
         self.frameGeometry().width()
     
-    def bgcolor(self):
-        pass
-        
+    def save_dialog(self):
+        file_filter = "Data File(*.xlsx *.csv *.dat);;Excel File(*.xlsx *.xls)"
+        file = QFileDialog.getSaveFileName(
+            parent=self, 
+            caption="select a data file",
+            directory= 'data file.dat',
+            filter = file_filter,
+            initialFilter="Excel File(*.xlsx *.xls)"
+        ) 
+        print(file)
 
-    def icon_setting(self):
-        # icon = ico.Icon('feather:loader',color=QtGui.QColor('salmon'),anim =anim)
-        # self.pushButton_3.setIcon(icon)
-        # # self.pushButton(icon)
-        # button = QpushButton 
-        # anim.start()     
+    def Q_checkBox(self):
+        print(self.QcheckBox.checkState())
+        if self.QcheckBox.checkState == 0:
+            self.labelA.setText("not selected")
+        else:
+            self.labelA.setText("selected")
+    def Q_checkBox_2(self):
+        if self.QcheckBox_2.checkState == 0:
+            self.labelb.setText("not selected")
+        else:
+            self.labelb.setText("selected")
+
+    def Q_checkBox_3(self):
+        if self.QcheckBox_3.checkState == 0:
+            self.labelc.setText("not selected")
+        else:
+            self.labelc.setText("selected")
+    def Q_checkBox_4(self):
+        if self.QcheckBox_4.checkState == 0:
+            self.labeld.setText("not selected")
+        else:
+            self.labeld.setText("selected")
+
+    def Q_checkBox_5(self):
+        if self.QcheckBox_5.checkState == 0:
+            self.labele.setText("not selected")
+        else:
+            self.labele.setText("selected")
+    def Q_checkBox_6(self):
+        if self.QcheckBox_6.checkState == 0:
+            self.labelf.setText("not selected")
+        else:
+            self.labelf.setText("selected")
+    def Q_checkBox_7(self):
+        if self.QcheckBox_7.checkState == 0:
+            self.labelg.setText("not selected")
+        else:
+            self.labelg.setText("selected")
+    def check_Bo_changed(self):
         pass
+    # def icon_setting(self):
+    #     # icon = ico.Icon('feather:loader',color=QtGui.QColor('salmon'),anim =anim)
+    #     # self.pushButton_3.setIcon(icon)
+    #     # # self.pushButton(icon)
+    #     # button = QpushButton 
+    #     # anim.start()     
+    #     pass
         
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
